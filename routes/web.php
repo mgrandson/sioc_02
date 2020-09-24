c7e04881530601f3605eca10dfde7bca49605be8
Misael Grandson <37724966+mgrandson@users.noreply.github.com>
2020-09-24 17:44:29 -0600

    Update web.php
    Actualizacion del archivo web.php

--- routes/web.php
+++ routes/web.php
@@ -27,22 +27,31 @@ Route::get('/home', 'HomeController@index')->name('home');
     return view('usuario/empleado');
 });*/
 
-Route::get('empleado', 'NavigationController@empleado')->name('empleado');
+Route::get('usuario', 'UsuarioController@usuario')->name('usuario');
 
+Route::get('proveedor', 'UsuarioController@usuarioProveedor')->name('proveedor');
 
+Route::post('/', 'UsuarioController@crearUsuario')->name('usuario.crear');
 
-Route::get('proveedor', 'NavigationController@proveedor')->name('proveedor');
+Route::get('/editar/{id}', 'UsuarioController@editarUsuario')->name('usuario.editar')->where('id', '[0-9]+');
 
+Route::put('/editar/{id}','UsuarioController@actualizarUsuario')->name('usuario.actualizar');
+
+Route::delete('/eliminar/{id}', 'UsuarioController@eliminarUsuario')->name('usuario.eleminar');
 
 /** MODULO DE COMPRAS - PROVEEDORES*/
 
-Route::get('compra', function(){
+Route::get('oferta/proveedor', 'ofertaController@ofertaProveedor')->name('ofertaProveedor');
+Route::get('oferta/tienda', 'ofertaController@ofertaTienda')->name('ofertaTienda');
+
+
+//Route::get('proveedor', 'NavigationController@proveedor')->name('proveedor');
+Route::get('compra', function () {
     return view('compra/oferta');
 });
 
-Route::get('mensaje', function(){
+Route::get('mensaje', function () {
     return view('compra/mensaje');
 });
 
 /** MODULO DE INVENTARIO */
-
