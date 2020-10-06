0a71c46f2d0a0c3c8e48ef51d583fd17ff6befd3
Misael Grandson <37724966+mgrandson@users.noreply.github.com>
2020-10-05 23:57:49 -0600

    Create sidebar.blade.php
    Contiene la estructura del menu lateral.

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,84 @@
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-dark sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('home') }}">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <!--i class="fas fa-laugh-wink"></i-->
+            <svg  width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-flower2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M8 16a4 4 0 0 0 4-4 4 4 0 0 0 0-8 4 4 0 0 0-8 0 4 4 0 1 0 0 8 4 4 0 0 0 4 4zm3-12a3 3 0 0 0-6 0c0 .073.01.155.03.247.544.241 1.091.638 1.598 1.084A2.987 2.987 0 0 1 8 5c.494 0 .96.12 1.372.331.507-.446 1.054-.843 1.598-1.084.02-.092.03-.174.03-.247zm-.812 6.052A2.99 2.99 0 0 0 11 8a2.99 2.99 0 0 0-.812-2.052c.215-.18.432-.346.647-.487C11.34 5.131 11.732 5 12 5a3 3 0 1 1 0 6c-.268 0-.66-.13-1.165-.461a6.833 6.833 0 0 1-.647-.487zm-3.56.617a3.001 3.001 0 0 0 2.744 0c.507.446 1.054.842 1.598 1.084.02.091.03.174.03.247a3 3 0 1 1-6 0c0-.073.01-.155.03-.247.544-.242 1.091-.638 1.598-1.084zm-.816-4.721A2.99 2.99 0 0 0 5 8c0 .794.308 1.516.812 2.052a6.83 6.83 0 0 1-.647.487C4.66 10.869 4.268 11 4 11a3 3 0 0 1 0-6c.268 0 .66.13 1.165.461.215.141.432.306.647.487zM8 9a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+            </svg>
+        </div>
+
+
+        <div class="sidebar-brand-text mx-3">{{ config('app.name', 'Laravel') }}</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="{{ route('home') }}">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Inicio</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Usuarios
+    </div>
+
+    <!-- Nav Item -->
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('usuario') }}">
+            <i class="fas fa-fw fa-user-cog"></i>
+            <span>Cuentas de Usuario</span></a>
+    </li>
+    <!-- Nav Item -->
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('proveedor') }}">
+            <i class="fas fa-fw fa-shipping-fast"></i>
+            <span>Proveedores</span></a>
+    </li>
+
+    <!-- Heading -->
+    <div class="sidebar-heading">Proveedores</div>
+    <!-- Nav Item -->
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('ofertaProveedor') }}">
+            <i class="fas fa-fw fa-file-invoice"></i>
+            <span>Ofertas [Proveedor]</span></a>
+    </li>
+     <!-- Nav Item -->
+     <li class="nav-item">
+        <a class="nav-link" href="{{ route('ofertaTienda') }}">
+            <i class="fas fa-fw fa-file-invoice"></i>
+            <span>Ofertas [Tienda]</span></a>
+    </li>
+    <!-- Nav Item -->
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('pedidoTienda') }}">
+            <i class="fas fa-fw fa-box"></i>
+            <span>Pedidos</span></a>
+    </li>
+    <!-- Nav Item -->
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('proveedor') }}">
+            <i class="fas fa-fw fa-envelope"></i>
+            <span>Mensajes</span></a>
+    </li>
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+</ul>
+<!-- End of Sidebar -->
