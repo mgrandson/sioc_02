8481ef1e380dacba5c3befe09e13a16ed4d3b8c0
Misael Grandson <37724966+mgrandson@users.noreply.github.com>
2020-10-08 06:01:49 -0600

    Update UsuarioController.php

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
@@ -21,23 +21,32 @@ class UsuarioController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('manager');
     }
+
     public function usuario()
     {
         //$usuarios = App\User::all();
+        $usuarios = App\User::paginate(3);
+        $roles = App\Role::all();
+
+        return view('usuario/usuario', compact('usuarios'), compact('roles'));
+    }
+
+    public function roles()
+    {
+        $usuarios = App\User::all();
         $usuariosTabla = App\User::paginate(3);
-        $usuarios = $this->convertirCodigoTexto($usuariosTabla);
         return view('usuario/usuario', compact('usuarios'));
     }
+
 
     public function usuarioProveedor()
     {
         //$usuarios = App\User::all();
-        $usuariosTabla = App\User::where('user_type', 2)->paginate(3);
+        $usuarios = App\User::where('role_id', 6)->paginate(3);
         //$usuarios = array();
 
-        $usuarios = $this->convertirCodigoTexto($usuariosTabla);
 
         return view('usuario/proveedor', compact('usuarios'));
     }
@@ -52,9 +61,7 @@ class UsuarioController extends Controller
             'last_name' => ['required', 'string'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'job' => 'required',
-            'user_type' => 'required',
-            'access_level' => 'required'
+            'role' => 'required',
         ]);
 
         $usuario = new App\User;
@@ -63,9 +70,7 @@ class UsuarioController extends Controller
         $usuario->email = $request->email;
 
         $usuario->password = Hash::make($request->password);
-        $usuario->job = $request->job;
-        $usuario->user_type = $request->user_type;
-        $usuario->access_level = $request->access_level;
+        $usuario->role_id = $request->role;
 
         $usuario->save();
         return back()->with('mensaje', 'Usuario registrado.');
@@ -74,7 +79,8 @@ class UsuarioController extends Controller
     public function editarUsuario($id)
     {
         $usuario = App\User::findOrFail($id);
-        return view('usuario.editarUsuario', compact('usuario'));
+        $roles = App\Role::all();
+        return view('usuario.editarUsuario', compact('usuario'), compact('roles'));
     }
 
     public function actualizarUsuario(Request $request, $id)
@@ -84,9 +90,7 @@ class UsuarioController extends Controller
             'name' => ['required', 'string'],
             'last_name' => ['required', 'string'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'job' => 'required',
-            'user_type' => 'required',
-            'access_level' => 'required'
+            'role' => 'required',
         ]);
 
         $usuario = App\User::findOrFail($id);
@@ -96,9 +100,7 @@ class UsuarioController extends Controller
         if ($usuario->password <> $request->password) {
             $usuario->password = Hash::make($request->password);
         }
-        $usuario->job = $request->job;
-        $usuario->user_type = $request->user_type;
-        $usuario->access_level = $request->access_level;
+        $usuario->role_id = $request->role;
 
         $usuario->save();
 
@@ -113,71 +115,4 @@ class UsuarioController extends Controller
         return back()->with('mensaje', 'Usuario' . $deletedUser . 'eliminado.');
     }
 
-
-
-    public function convertirCodigoTexto($usuarios)
-    {
-        /*
-        2. Administrador
-        3. Gerente
-        5. Contador
-        7. Cajero
-        11. Vendedor
-        13. Distribuidor*/
-
-        for ($i = 0; $i < count($usuarios); $i++) {
-            $u = $usuarios[$i];
-            if ($u['job'] == 2) {
-                $u['job'] = 'Administrador';
-            } elseif ($u['job'] == 3) {
-                $u['job'] = 'Gerente';
-            } elseif ($u['job'] == 5) {
-                $u['job'] = 'Contador';
-            } elseif ($u['job'] == 7) {
-                $u['job'] = 'Cajero';
-            } elseif ($u['job'] == 11) {
-                $u['job'] = 'Vendedor';
-            } elseif ($u['job'] == 13) {
-                $u['job'] = 'Distribuidor';
-            } else {
-                $u['job'] = 'Indefinido';
-            }
-
-            if ($u['user_type'] == 1) {
-                $u['user_type'] = 'Empleado';
-            } elseif ($u['user_type'] == 2) {
-                $u['user_type'] = 'Proveedor';
-            } else {
-                $u['user_type'] = 'Indefinido';
-            }
-
-            /*
-            1. Alpha
-            2. Beta
-            3. Gamma
-            4. Delta
-            5. Epsilon
-            6. Dseta
-*/
-            if ($u['access_level'] == 1) {
-                $u['access_level'] = 'Alpha';
-            } elseif ($u['access_level'] == 2) {
-                $u['access_level'] = 'Beta';
-            }elseif ($u['access_level'] == 3) {
-                $u['access_level'] = 'Gamma';
-            }elseif ($u['access_level'] == 4) {
-                $u['access_level'] = 'Delta';
-            }elseif ($u['access_level'] == 5) {
-                $u['access_level'] = 'Epsilon';
-            }elseif ($u['access_level'] == 6) {
-                $u['access_level'] = 'Dseta';
-            } else {
-                $u['access_level'] = 'Indefinido';
-            }
-
-            $usuarios[$i] = $u;
-        }
-
-        return $usuarios;
-    }
 }
