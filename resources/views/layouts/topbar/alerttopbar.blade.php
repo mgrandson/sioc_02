0c37a48062038886bd0bebd3e9832828cbb1b49d
Misael Grandson <37724966+mgrandson@users.noreply.github.com>
2020-10-08 05:56:58 -0600

    Update topbar
    Separando las etiquetas html y el codigo embedido en diferentes archivos para falicitar la edicion.

---
+++ resources/views/layouts/topbar/alerttopbar.blade.php
@@ -0,0 +1,50 @@
+<!-- Nav Item - Alerts -->
+<li class="nav-item dropdown no-arrow mx-1">
+    <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button"
+        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <i class="fas fa-bell fa-fw"></i>
+        <!-- Counter - Alerts -->
+        <span class="badge badge-danger badge-counter">3+</span>
+    </a>
+    <!-- Dropdown - Alerts -->
+    <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
+        aria-labelledby="alertsDropdown">
+        <h6 class="dropdown-header">
+            Notificaciones
+        </h6>
+        <a class="dropdown-item d-flex align-items-center" href="#">
+            <div class="mr-3">
+                <div class="icon-circle bg-dark">
+                    <i class="fas fa-file-alt text-white"></i>
+                </div>
+            </div>
+            <div>
+                <div class="small text-gray-500">Septiembre 29, 2020</div>
+                <span class="font-weight-bold">Se ha registrado una venta.</span>
+            </div>
+        </a>
+        <a class="dropdown-item d-flex align-items-center" href="#">
+            <div class="mr-3">
+                <div class="icon-circle bg-success">
+                    <i class="fas fa-shipping-fast text-white"></i>
+                </div>
+            </div>
+            <div>
+                <div class="small text-gray-500">Septiembre 28, 2020</div>
+                El proveedor a modificado una oferta.
+            </div>
+        </a>
+        <a class="dropdown-item d-flex align-items-center" href="#">
+            <div class="mr-3">
+                <div class="icon-circle bg-info">
+                    <i class="fas fa-store text-white"></i>
+                </div>
+            </div>
+            <div>
+                <div class="small text-gray-500">Septiembre 27, 2020</div>
+                Nuevos Productos Registrados.
+            </div>
+        </a>
+        <a class="dropdown-item text-center small text-gray-500" href="#">Todas las notificaciones.</a>
+    </div>
+</li>
---
+++ resources/views/layouts/topbar/topbar.blade.php
@@ -0,0 +1,70 @@
+ <!-- Topbar -->
+ <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow border-bottom-secondary">
+     <!-- Sidebar Toggle (Topbar) -->
+     <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+         <i class="fa fa-bars"></i>
+     </button>
+        <div>
+            <h5 class="m-0 font-weight-bold text-gray d-none d-sm-none d-md-block">
+                {{ config('app.business', 'CR')    }}
+             </h5>
+
+             <h5 class="m-0 font-weight-bold text-dark d-block d-sm-block d-md-none">
+                CR
+            </h5>
+        </div>
+
+     <div class="topbar-divider d-none d-sm-block"></div>
+
+     <!-- Topbar Search -->
+     <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+         <div class="input-group">
+             <input type="text" class="form-control bg-light border-0 small" placeholder="Buscar..." aria-label="Search"
+                 aria-describedby="basic-addon2">
+             <div class="input-group-append">
+                 <button class="btn btn-dark" type="button">
+                     <i class="fas fa-search fa-sm"></i>
+                 </button>
+             </div>
+         </div>
+     </form>
+
+     <!-- Topbar Navbar -->
+     <ul class="navbar-nav ml-auto">
+         <!-- Nav Item - Search Dropdown (Visible Only XS Movil) -->
+         <li class="nav-item dropdown no-arrow d-sm-none">
+
+            <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
+                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-search fa-fw"></i>
+            </a>
+         <!-- Dropdown - Messages -->
+         <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
+                aria-labelledby="searchDropdown">
+                <form class="form-inline mr-auto w-100 navbar-search">
+                    <div class="input-group">
+                        <input type="text" class="form-control bg-light border-0 small"
+                            placeholder="Search for..." aria-label="Search"
+                            aria-describedby="basic-addon2">
+                        <div class="input-group-append">
+                            <button class="btn btn-dark" type="button">
+                                <i class="fas fa-search fa-sm"></i>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </li>
+
+         <!-- Nav Item - Alerts -->
+         @include('layouts.topbar.alerttopbar')
+
+         <!-- Nav Item - Messages -->
+         @include('layouts.topbar.msgtopbar')
+
+         <!-- Nav Item - User Information -->
+         @include('layouts.topbar.usertopbar')
+
+     </ul>
+ </nav>
+ <!-- End of Topbar -->
