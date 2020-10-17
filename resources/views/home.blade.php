9aff263c81f5aa6cab9e22c27141c26a0dce79cf
Misael Grandson <37724966+mgrandson@users.noreply.github.com>
2020-10-17 12:13:11 -0600

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,9 +6,7 @@
         <div class="container-fluid">
             <!-- Page Heading -->
             <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">{{config('app.business', 'CR')}}.</h1>
-              <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generar Reporte</a>
+            <h1 class="h3 mb-0 text-gray-800">Bienvenido, {{ Auth::user()->name." ".Auth::user()->last_name}}.</h1>
             </div>
-  
         </div>
 @endsection
