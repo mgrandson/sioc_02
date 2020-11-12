7c5809dda576e32862a81b460c187eb0a1d6f955
Misael Grandson <37724966+mgrandson@users.noreply.github.com>
2020-11-12 00:43:22 -0600

    Update Item.php
    Se agrego la funcion photos para obtener las fotos asociadas al item de la oferta.

--- app/Item.php
+++ app/Item.php
@@ -9,4 +9,8 @@ class Item extends Model
     public function offers(){
         return $this->belongsToMany(Offer::class)->withTimestamps();
     }
+
+    public function photos(){
+        return $this->hasMany(Photo::class);
+    }
 }
