13b58c46f59f7be2261410b8ebb86f4b21098e1c
Misael Grandson <37724966+mgrandson@users.noreply.github.com>
2020-11-12 00:38:00 -0600

    Create 2020_10_18_005809_create_photos_table.php
    Migración para crear la trabla fotos en la base de datos.

---
+++ database/migrations/2020_10_18_005809_create_photos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhotosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photos', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('item_id')->unsigned();
+            $table->foreign('item_id')->references('id')->on('items')->onDelete('cascade')->onUpdate('cascade');
+            $table->String('filename');
+            $table->String('path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photos');
+    }
+}
