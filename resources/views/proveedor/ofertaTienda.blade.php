2f70b3c43e6976d6b620380079ddf824f7b5c8f9
Misael Grandson <37724966+mgrandson@users.noreply.github.com>
2020-11-15 13:01:38 -0600

    Update ofertaTienda.blade.php
    Se actualizo la vista para ver el historial de ofertas del lado del gerente.

--- resources/views/proveedor/ofertaTienda.blade.php
+++ resources/views/proveedor/ofertaTienda.blade.php
@@ -7,26 +7,6 @@
     <p class="mb-4"></p>
     <!-- DataTabla Ofertas -->
     <div class="card shadow mb-4">
-        <div class="card-header py-2">
-            <h6 class="m-0 font-weight-bold text-dark">Oferta</h6>
-        </div>
-        <div class="card-body">
-            <div class="form-group text-center table-responsive" style="height: 250px;">
-
-                    <img src="{{ asset('img/shoe_01.png') }}"  id="myImg" class="mh-100 img-fluid img-thumbnail" alt="...">
-
-                    <img src="{{ asset('img/shoe_02.png') }}" id="myImg" class="mh-100 img-fluid img-thumbnail" alt="...">
-
-                    <img src="{{ asset('img/shoe_03.png') }}" id="myImg" class="mh-100 img-fluid img-thumbnail" alt="...">
-
-                    <img src="{{ asset('img/shoe_04.png') }}" id="myImg" class="mh-100 img-fluid img-thumbnail" alt="...">
-            </div>
-        </div>
-    </div>
-
-
-    <!-- DataTabla Ofertas -->
-    <div class="card shadow mb-4">
         <div class="card-header py-3">
             <h6 class="m-0 font-weight-bold text-dark">Historial de Ofertas</h6>
         </div>
@@ -44,54 +24,25 @@
                             <th>Acciones</th>
                         </tr>
                     </thead>
-                    <tfoot>
-                        <tr>
-                            <td>#</td>
-                            <th>Nombre</th>
-                            <th>Descripcion</th>
-                            <th>Proveedor</th>
-                            <th>Fecha</th>
-                            <th>Estado</th>
-                            <th>Acciones</th>
-                        </tr>
-                    </tfoot>
                     <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Oferta A</td>
-                            <td>Descripcion A</td>
-                            <td>Proveedor 01</td>
-                            <td>25/09/2020</td>
-                            <td>Pendiente</td>
-                            <td>x</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Oferta B</td>
-                            <td>Descripcion B</td>
-                            <td>Proveedor 02</td>
-                            <td>20/09/2020</td>
-                            <td>Revisada</td>
-                            <td>x</td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td>Oferta C</td>
-                            <td>Descripcion C</td>
-                            <td>Proveedor 03</td>
-                            <td>15/09/2020</td>
-                            <td>Revisada</td>
-                            <td>x</td>
-                        </tr>
-                        <tr>
-                            <td>4</td>
-                            <td>Oferta D</td>
-                            <td>Descripcion D</td>
-                            <td>Proveedor 04</td>
-                            <td>15/09/2020</td>
-                            <td>Revisada</td>
-                            <td>x</td>
-                        </tr>
+                        @foreach ($offers as $offer)
+                            <tr>
+                                <td>{{ $offer->correlative }}</td>
+                                <td>{{ $offer->code }}</td>
+                                <td></td>
+                                <td>{{ $offer->business['name'] }}</td>
+                                <td>{{ date('d-m-Y', strtotime($offer->created_at)) }}</td>
+                                <td>{{ $offer->offer_status }}</td>
+                                <td>
+                                    <a class="nav-link d-inline" href="{{ route('verOfertaTienda', $offer->id) }}">
+                                        <i class="fas fa-sm fa-eye"> Ver </i>
+                                    </a>
+    
+
+    
+                                </td>
+                            </tr>
+                        @endforeach
                 </table>
             </div>
         </div>
