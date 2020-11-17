fc4c74d58ecb8aa4a4b853f117c7613d318dfc63
Misael Grandson <37724966+mgrandson@users.noreply.github.com>
2020-11-16 18:10:16 -0600

    Update Business.php
    Se agrego la función offers() para relacionar el modelo.

--- app/Business.php
+++ app/Business.php
@@ -11,4 +11,8 @@ class Business extends Model
     public function user(){
         return $this->belongsTo('App\User');
     }
+
+    public function offers(){
+        return $this->belongsToMany('App\Offer')->withTimestamps();
+    }
 }
