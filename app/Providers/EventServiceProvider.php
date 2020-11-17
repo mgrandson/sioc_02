21116fe68a3e0f2c9dd510ee818b534fd66cafc3
Misael Grandson <37724966+mgrandson@users.noreply.github.com>
2020-11-16 18:12:51 -0600

    Update EventServiceProvider.php

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -7,6 +7,9 @@ use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
 
+use App\Events\OfferEvent;
+use App\Listeners\OfferListener;
+
 class EventServiceProvider extends ServiceProvider
 {
     /**
@@ -17,6 +20,9 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+        OfferEvent::class =>[
+            OfferListener::class,
         ],
     ];
 
